b880f7cfb4947aaf4b8cd023077bdf8b8f8c08c0
Winston <44872771+winston-yallow@users.noreply.github.com>
2023-12-01 23:16:31 +0100

    Use EditorInterface singleton instead of method

--- addons/aoc/plugin.gd
+++ addons/aoc/plugin.gd
@@ -11,7 +11,7 @@ func _enter_tree() -> void:
 	dock = DOCK_SCENE.instantiate()
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_UL, dock)
 	dock.setup()
-	dock.filesystem_changed.connect(get_editor_interface().get_resource_filesystem().scan)
+	dock.filesystem_changed.connect(EditorInterface.get_resource_filesystem().scan)
 
 
 func _exit_tree() -> void:
